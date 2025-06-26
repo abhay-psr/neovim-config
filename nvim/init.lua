41fb1ffc56700c85d19ab8579d292dfbde1d547c
abhay-psr <a6hay.c.psr@gmail.com>
2025-06-26 23:23:37 +0530

    install vim-plug

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,3 +20,14 @@ vim.cmd.set('softtabstop=4')
 vim.cmd.set('shiftwidth=4')
 vim.cmd.set('syntax=on')
 -- }
+
+-- {
+-- Plugin Manager
+-- vim-plug
+-- doc: https://github.com/junegunn/vim-plug
+-- install doc: https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+    Plug 'nvim-treesitter/nvim-treesitter'
+vim.call('plug#end')
+-- }
